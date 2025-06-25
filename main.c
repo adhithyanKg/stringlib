99e080dcd1441ff7dba9082bf8a89d1d304d62eb
Adhithyan KG <159514247+adhithyanKg@users.noreply.github.com>
2025-06-24 22:19:12 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "stringlib.h"
+
+// int main()
+// {
+//     char string[] = "Adhithyan is \n great";
+    
+//     printf("Number of words in the string %d\n", wordCount(string));
+    
+//     return 0;
+// }
