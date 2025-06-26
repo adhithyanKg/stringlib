61e2ee77b628e21f888f084fa7ce2da2ac559f56
Adhithyan KG <159514247+adhithyanKg@users.noreply.github.com>
2025-06-25 22:04:14 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,37 @@
 #include "stringlib.h"
+#include "config.h"
 
-// int main()
-// {
-//     char string[] = "Adhithyan is \n great";
+#ifdef RUN_MAIN
+
+int main()
+{
+    char string[] = "Albe i eblA";
+    char tempBuffer[100];
     
-//     printf("Number of words in the string %d\n", wordCount(string));
+    strcpy(tempBuffer, string);
+
+    printf("Number of words in the string %d\n", wordCount(string));
+    printf("Last Character Index  %d\n", findLastCharacterIndex(string, 't'));
+    printf("First Character Index  %d\n", findFirstCharacterIndex(string, 't'));
     
-//     return 0;
-// }
+    convertToUpperCase(tempBuffer);
+    printf("String in Uppercase %s\n", tempBuffer);
+    convertToLowerCase(tempBuffer);
+    printf("String in Lowercase %s\n", tempBuffer);
+    
+    printf("Number of spaces in the string %d\n", countSpace(string));
+    printf("Number of vowels in the string %d\n", countVowelsInTheString(string));
+    
+    printf("Occurence of the character '%c' without case sensitive is %d\n", 'a', countCharacterIgnoreCaseSensitive(string, 'a'));
+    printf("Occurence of the character '%c' with case sensitive is %d\n", 'a', countCharacterCaseSensitive(string, 'a'));
+    
+    char *reverse = reverseString(string);
+    printf("Originial string: %s --> Reversed string: %s\n", tempBuffer, reverse);
+    free(reverse);
+    
+    printf("Is string '%s' palindrome? %s\n", string, isPalindrome(string) ? "yes" : "no");
+    
+    return 0;
+}
+
+#endif
