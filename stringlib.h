41ff8b043fd1fce5382dd0f882c2937502adedc1
Adhithyan KG <159514247+adhithyanKg@users.noreply.github.com>
2025-06-25 22:05:02 -0400

    Update stringlib.h

--- stringlib.h
+++ stringlib.h
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int wordCount(const char* string);
 int countVowelsInTheString(const char* string);
+char* reverseString(const char* string);
+bool isPalindrome(const char* string);
+int countSpace(const char* string);
+static int findCharacterIndex(const char* string, char c, bool isFirstIndex);
+int findFirstCharacterIndex(const char* string, const char c);
+int findLastCharacterIndex(const char* string, const char c);
+void convertToLowerCase(char* string);
+void convertToUpperCase(char* string);
+static int countCharacter(const char* string, char c, bool isNotCaseSensitive);
+int countCharacterIgnoreCaseSensitive(const char* string, char c);
+int countCharacterCaseSensitive(const char* string, char c);
